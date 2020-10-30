5b666ae5fb20b17304145d9b7510e2002adcbb91
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-10-30 13:24:46 -0400

    add unit tests for some of the things mentioned in the readme

--- assuo/tests/algo.rs
+++ assuo/tests/algo.rs
@@ -201,3 +201,144 @@ async fn mixed_pre_and_post_inserts_are_in_order() -> Result<(), Box<dyn std::er
     assert_eq!(&patched, &"Hello, World!".as_bytes());
     Ok(())
 }
+
+// == CODE SAMPLE TESTS ==
+// if there is an assuo config file on the documentation, it should be copied here to ensure it works.
+// old tests shouldn't get removed (unless there is a `major` version upgrade) to ensure no regressions.
+
+async fn helper<B: AsRef<[u8]>>(
+    expected: B,
+    source: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let source = assuo::models::try_parse(source)?;
+    let result = do_patch(source).await?;
+    assert_eq!(expected.as_ref(), result.as_slice());
+    Ok(())
+}
+
+#[tokio::test]
+async fn readme_1() -> Result<(), Box<dyn std::error::Error>> {
+    helper(
+        "Hello, World!",
+        r#"
+# The source specifies where we get a copy of the source bytes from.
+# In our case, we will specify some `text`.
+[source]
+text = "Hello!"
+
+# Now, we can apply a series of patches. In TOML, the double brackets
+# ([[]]) mean an array, so we could add more of these sequentially in
+# the file later.
+[[patch]]
+
+# We specify what we want to do. Here, we'll say that we want to insert
+# some text. We'll make the source go from "Hello!" to "Hello, World!"
+do = "insert"
+
+# Way means the direction we insert from. In our small example, this
+# doesn't matter, but we'll cover it more later.
+way = "post"
+
+# The spot is where we want to insert at. We can find out where we want
+# to insert at based on the index in the source bytes.
+#
+# | H | e | l | l | o | ! |
+# ^   ^   ^   ^   ^   ^   ^
+# 0   1   2   3   4   5   6
+spot = 5
+
+# Now, we have to supply the source for where we get the bytes from.
+# You may notice that this looks very similar to the [source] we have at
+# the top, and you'd be absolutely right! Any valid [source] is a valid source
+# here too.
+source = { text = ", World" }
+"#,
+    )
+    .await
+}
+
+#[tokio::test]
+async fn readme_2() -> Result<(), Box<dyn std::error::Error>> {
+    helper(
+        vec![1, 2, 3, 4],
+        r#"
+[source]
+bytes = [1, 2, 3, 4]
+"#,
+    )
+    .await
+}
+
+#[tokio::test]
+async fn readme_3() -> Result<(), Box<dyn std::error::Error>> {
+    helper(
+        "Hello!",
+        r#"
+[source]
+text = "Hello!"
+"#,
+    )
+    .await
+}
+
+// TODO: test loading files
+// TODO: test loading URL
+// TODO: test loading assuo files
+// TODO: test loading assuo URL
+
+#[tokio::test]
+async fn readme_8() -> Result<(), Box<dyn std::error::Error>> {
+    helper(
+        ">ba<",
+        r#"
+[source]
+text = "><"
+
+[[patch]]
+do = "insert"
+way = "post"
+spot = 1
+source = { text = "a" }
+
+[[patch]]
+do = "insert"
+way = "post"
+spot = 1
+source = { text = "b" }
+"#,
+    )
+    .await
+}
+
+#[tokio::test]
+async fn readme_9() -> Result<(), Box<dyn std::error::Error>> {
+    helper(
+        ">bac<",
+        r#"
+[source]
+text = "><"
+
+[[patch]]
+do = "insert"
+way = "post"
+spot = 1
+source = { text = "a" }
+
+[[patch]]
+do = "insert"
+way = "post"
+spot = 1
+source = { text = "b" }
+
+[[patch]]
+do = "insert"
+way = "pre"
+spot = 1
+source = { text = "c" }
+"#,
+    )
+    .await
+}
+
+// == PREVENTING REGRESSION TESTS ==
+// if there is an issue posted, a test should be placed after this point to ensure that there will be no regression in the future
